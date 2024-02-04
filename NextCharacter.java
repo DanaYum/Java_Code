5f51bd1cebe1c030b4815c1a3457aab3560a90d6
Dhana <dhanachandrayumnam12@gmail.com>
2024-02-05 04:52:38 +0530

    just added

---
+++ NextCharacter.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class NextCharacter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a character (a to z): ");
+        char inputChar = scanner.next().charAt(0);
+
+        if ((inputChar >= 'a' && inputChar < 'z')||(inputChar>='A'&& inputChar<'Z' )){
+            char nextChar = (char) (inputChar + 1);
+            char preChar= (char) (inputChar -1);
+            System.out.println("Next character: " + nextChar);
+            System.out.println("Previous character: "+ preChar);
+        } else {
+            System.out.println("Invalid input. Please enter a character between 'a' and 'z'.");
+        }
+
+
+        scanner.close();
+    }
+}
